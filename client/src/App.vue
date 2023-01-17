0ce7568e136465c606af11e9a8b8e26481a9c031
Jorge Atempa <atempaitm@gmail.com>
2023-01-17 14:32:33 -0800

    Create 'CustomInput' component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import CustomInput from './components/CustomInput.vue';
 import Header from './components/Header.vue';
 </script>
 
 <template>
   <Header>Temperature Converter</Header>
+  <CustomInput />
 </template>
 
 <style scoped></style>
---
+++ client/src/components/CustomInput.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data: () => ({
+    temperatureVal: '',
+  }),
+  emits: ['handle-change'],
+  methods: {
+    getTemperatureVal() {
+      this.$emit('handle-change', this.temperatureVal);
+      this.temperatureVal = '';
+    },
+  },
+};
+</script>
+
+<template>
+  <input
+    :value="temperatureVal"
+    @input="temperatureVal = $event.target.value"
+    @keyup.enter="getTemperatureVal"
+    placeholder="Enter the temperature"
+  />
+</template>
+
+<style scoped>
+input {
+  height: 50px;
+  width: 100%;
+  font-size: 2rem;
+}
+</style>
