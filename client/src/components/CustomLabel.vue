862230e4028188ad3b351a71014c5f77611ad5f4
Jorge Atempa <atempaitm@gmail.com>
2023-01-17 19:05:57 -0800

    Create 'CustomLabel' component

---
+++ client/src/components/CustomLabel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="result">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+#result {
+  background-color: white;
+  margin: 0.75em auto;
+  padding: 0.25em;
+  width: 100%;
+  font-size: 2rem;
+  text-align: center;
+  vertical-align: middle;
+}
+</style>
