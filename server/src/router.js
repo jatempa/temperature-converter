f6c7c9034b0cd41cad6ca09a8ae43982a1851446
Jorge Atempa <atempaitm@gmail.com>
2023-01-17 19:59:57 -0800

    Create endpoints to convert celsius to fahrenheit and vice versa

--- server/src/router.js
+++ server/src/router.js
@@ -2,9 +2,42 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.get('/custom', (req, res) => {
-  res.status(200);
-  res.send({ message: 'hello from custom route' });
+const errorMessage = 'Invalid value, try again !';
+
+const response = {
+  result: '',
+  error: '',
+};
+
+router.get('/celsius-to-fahrenheit', (req, res) => {
+  const { temperature } = req.body;
+
+  try {
+    if (!temperature || typeof temperature !== 'number') {
+      throw new Error(errorMessage);
+    }
+
+    const result = (temperature * 9) / 5 + 32;
+    res.status(200).send({ ...response, result });
+  } catch (e) {
+    console.log(e);
+    res.status(404).send({ ...response, error: `${e.name}: ${e.message}` });
+  }
+});
+
+router.get('/fahrenheit-to-celsius', (req, res) => {
+  const { temperature } = req.body;
+
+  try {
+    if (!temperature || typeof temperature !== 'number') {
+      throw new Error(errorMessage);
+    }
+
+    const result = ((temperature - 32) * 5) / 9;
+    res.status(200).send({ ...response, result });
+  } catch (e) {
+    res.status(404).send({ ...response, error: `${e.name}: ${e.message}` });
+  }
 });
 
 module.exports = router;
