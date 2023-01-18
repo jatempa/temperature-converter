0951e8315a06e4bdc44c149b79b2da5ebb162fc0
Jorge Atempa <atempaitm@gmail.com>
2023-01-17 20:05:38 -0800

    Enable 'cors' dependency

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const router = require('./router');
 
 // CONSTANTS
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api', router);
 
