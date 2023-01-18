847000b723b138401d3e714726f47e4ad710bdef
Jorge Atempa <atempaitm@gmail.com>
2023-01-17 17:59:30 -0800

    Create 'CustomSelect' component

---
+++ client/src/components/CustomSelect.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  data() {
+    return {
+      units: ['°C', '°F'],
+    };
+  },
+};
+</script>
+
+<template>
+  <select
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  >
+    <option v-for="unit in units" :key="unit">{{ unit }}</option>
+  </select>
+</template>
+
+<style scoped>
+select {
+  flex-basis: 10%;
+  height: inherit;
+  font-size: 2rem;
+}
+</style>
